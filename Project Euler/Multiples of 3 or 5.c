74cdf2f0f88d8501c76449a7fdd21746f6ff7c99
jarpex <default@jarpex.ru>
2022-01-11 02:35:35 +0300

    Add project files.

---
+++ Project Euler/Multiples of 3 or 5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int result = 0;
+	for (int i = 1; i < 1000; ++i) {
+		if ((i % 3 == 0) || (i % 5 == 0)) {
+			result += i;
+		}
+	}
+	printf("The result is %d.\n", result);
+	return 0;
+}
